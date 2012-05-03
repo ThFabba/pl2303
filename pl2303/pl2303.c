1559bf2f9784a9d1681bd2ac823128b57a1dd79e
thfabba <thfabba@localhost>
2012-05-03 21:59:17 +0200

    Add required driver routines

--- pl2303/pl2303.c
+++ pl2303/pl2303.c
@@ -1,9 +1,19 @@
 #include <Ntddk.h>
 
 DRIVER_INITIALIZE DriverEntry;
+static DRIVER_UNLOAD Pl2303Unload;
+static DRIVER_ADD_DEVICE Pl2303AddDevice;
+__drv_dispatchType(IRP_MJ_PNP)
+static DRIVER_DISPATCH Pl2303DispatchPnp;
+__drv_dispatchType(IRP_MJ_POWER)
+static DRIVER_DISPATCH Pl2303DispatchPower;
 
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text(INIT, DriverEntry)
+#pragma alloc_text(PAGE, Pl2303Unload)
+#pragma alloc_text(PAGE, Pl2303AddDevice)
+#pragma alloc_text(PAGE, Pl2303DispatchPnp)
+#pragma alloc_text(PAGE, Pl2303DispatchPower)
 #endif /* defined ALLOC_PRAGMA */
 
 NTSTATUS
@@ -14,6 +24,67 @@ DriverEntry(
 {
     UNREFERENCED_PARAMETER(DriverObject);
     UNREFERENCED_PARAMETER(RegistryPath);
+
+    PAGED_CODE();
+
+    DriverObject->DriverUnload = Pl2303Unload;
+    DriverObject->DriverExtension->AddDevice = Pl2303AddDevice;
+    DriverObject->MajorFunction[IRP_MJ_PNP] = Pl2303DispatchPnp;
+    DriverObject->MajorFunction[IRP_MJ_POWER] = Pl2303DispatchPower;
+
+    return STATUS_SUCCESS;
+}
+
+static
+VOID
+NTAPI
+Pl2303Unload(
+    _In_ PDRIVER_OBJECT DriverObject)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+
+    PAGED_CODE();
+}
+
+static
+NTSTATUS
+NTAPI
+Pl2303AddDevice(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PDEVICE_OBJECT PhysicalDeviceObject)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(PhysicalDeviceObject);
+
+    PAGED_CODE();
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static
+NTSTATUS
+NTAPI
+Pl2303DispatchPnp(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _Inout_ PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+    UNREFERENCED_PARAMETER(Irp);
+
+    PAGED_CODE();
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static
+NTSTATUS
+NTAPI
+Pl2303DispatchPower(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _Inout_ PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+    UNREFERENCED_PARAMETER(Irp);
 
     PAGED_CODE();
 
