b28be93b560edea850c5ec7cd58570344965756b
thfabba <thfabba@localhost>
2012-05-26 00:01:39 +0200

    Print PnP minor function for debugging

--- pl2303/pnp.c
+++ pl2303/pnp.c
@@ -368,6 +368,46 @@ Pl2303AddDevice(
     return STATUS_SUCCESS;
 }
 
+static const PCSTR PnpMinorFunctionNames[] =
+{
+    "IRP_MN_START_DEVICE",
+    "IRP_MN_QUERY_REMOVE_DEVICE",
+    "IRP_MN_REMOVE_DEVICE",
+    "IRP_MN_CANCEL_REMOVE_DEVICE",
+    "IRP_MN_STOP_DEVICE",
+    "IRP_MN_QUERY_STOP_DEVICE",
+    "IRP_MN_CANCEL_STOP_DEVICE",
+    "IRP_MN_QUERY_DEVICE_RELATIONS",
+    "IRP_MN_QUERY_INTERFACE",
+    "IRP_MN_QUERY_CAPABILITIES",
+    "IRP_MN_QUERY_RESOURCES",
+    "IRP_MN_QUERY_RESOURCE_REQUIREMENTS",
+    "IRP_MN_QUERY_DEVICE_TEXT",
+    "IRP_MN_FILTER_RESOURCE_REQUIREMENTS",
+    "0x0E",
+    "IRP_MN_READ_CONFIG",
+    "IRP_MN_WRITE_CONFIG",
+    "IRP_MN_EJECT",
+    "IRP_MN_SET_LOCK",
+    "IRP_MN_QUERY_ID",
+    "IRP_MN_QUERY_PNP_DEVICE_STATE",
+    "IRP_MN_QUERY_BUS_INFORMATION",
+    "IRP_MN_DEVICE_USAGE_NOTIFICATION",
+    "IRP_MN_SURPRISE_REMOVAL",
+    "IRP_MN_QUERY_LEGACY_BUS_INFORMATION",
+    "IRP_MN_DEVICE_ENUMERATED"
+};
+
+static
+PCSTR
+Pl2303GetPnpMinorFunctionName(
+    _In_ UCHAR MinorFunction)
+{
+    if (MinorFunction < sizeof(PnpMinorFunctionNames) / sizeof(PnpMinorFunctionNames[0]))
+        return PnpMinorFunctionNames[MinorFunction];
+    return "Unknown";
+}
+
 NTSTATUS
 NTAPI
 Pl2303DispatchPnp(
@@ -397,6 +437,10 @@ Pl2303DispatchPnp(
         IoCompleteRequest(Irp, IO_NO_INCREMENT);
         return Status;
     }
+
+    Pl2303Debug(         "%s. MinorFunction=%s (0x%x)\n",
+                __FUNCTION__, Pl2303GetPnpMinorFunctionName(IoStack->MinorFunction),
+                              IoStack->MinorFunction);
 
     switch (IoStack->MinorFunction)
     {
