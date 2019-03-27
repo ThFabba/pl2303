62b8c6145313c83fea3358b631c8de7f6fbd319d
Thomas Faber <thomas.faber@reactos.org>
2019-03-27 21:49:17 +0100

    Add LGPL license headers.

--- pl2303/pnp.c
+++ pl2303/pnp.c
@@ -1,3 +1,22 @@
+/*
+ * PL2303 Driver PnP routines
+ * Copyright (C) 2012-2019  Thomas Faber
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "pl2303.h"
 
 static NTSTATUS Pl2303InitializeDevice(_In_ PDEVICE_OBJECT DeviceObject,
@@ -275,7 +294,7 @@ Pl2303StartDevice(
                                             FALSE);
             return Status;
         }
-        
+
         /* FIXME */
 #if 0
         Status = RtlWriteRegistryValue(RTL_REGISTRY_DEVICEMAP,
--- pl2303/queue.c
+++ pl2303/queue.c
@@ -1,3 +1,22 @@
+/*
+ * PL2303 Driver request queue routines
+ * Copyright (C) 2012-2019  Thomas Faber
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "pl2303.h"
 
 _Function_class_(IO_CSQ_INSERT_IRP_EX)
@@ -86,7 +105,7 @@ Pl2303QueueInsertIrp(
     _In_ PVOID InsertContext)
 {
     PQUEUE Queue = CONTAINING_RECORD(Csq, QUEUE, Csq);
-    
+
     UNREFERENCED_PARAMETER(InsertContext);
 
     ASSERT(KeGetCurrentIrql() == DISPATCH_LEVEL);
